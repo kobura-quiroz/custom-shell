00ac2027cea8d2c61b257b97b8bfa0ce040d1052
ivan-quiroz-unosquare <ivan.quiroz@unosquare.com>
2026-03-17 20:01:56 -0600

    Handle invalid commands

--- shell/Program.cs
+++ shell/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main()
     {
         Console.Write("$ ");
+        var command = Console.ReadLine();
+        Console.WriteLine($"{command}: command not found");
     }
 }
